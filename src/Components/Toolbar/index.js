2ff7fee5fd657c2caadfb4f313601f519f472f61
Abu Huraira Bin Aman <shams.bd71@gmail.com>
2016-02-24 12:49:35 +0600

    test app added

--- src/Components/Toolbar/index.js
+++ src/Components/Toolbar/index.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
 
 class Toolbar extends Component {
+  //onClick={this.addLike.bind(this)}
+  increseLike()
+  {
+    this.props.addLike(parseInt(this.refs.likeBox.innerHTML), this.props.statusIndex);
+  }
+
+
   render() {
     return (
-      <small><a className="button">
-        <i className="fa fa-pencil" /> Like</a>
+      <small>
+        <a className="button" onClick={this.increseLike.bind(this)}>
+          <i ref="likeBox">{this.props.likes}</i> Like
+        </a>
         <a className="button"><i className="fa fa-pencil" /> Reply</a>
       </small>
     );
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import 'font-awesome/css/font-awesome.min.css';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './Components/App';
+import Js from './Js';
 
+//render(<Js items={['Apple', 'Banana', 'Cranberry']} />, document.getElementById('content-wrapper'));
 render(<App />, document.getElementById('content-wrapper'));
